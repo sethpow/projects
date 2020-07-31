36364efa0d4a445974159a8b36c26b9f2dee4790
Seth Powelson <sethmadspowelson@gmail.com>
2020-07-31 16:51:42 -0600

    updates

--- React/fit-bit-app/src/components/EditExercise.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EditExercise() {
-    return (
-        <div>
-            edit exercise
-        </div>
-    )
-}
--- React/fit-bit-app/src/components/ExerciseList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ExerciseList() {
-    return (
-        <div>
-            exercise list
-        </div>
-    )
-}
