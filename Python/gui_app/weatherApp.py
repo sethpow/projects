83864f9eb4e87c50ad6f3d0ca6e4e0ee604f221d
Seth Powelson <sethmadspowelson@gmail.com>
2020-08-04 11:37:41 -0600

    connected to API

--- Python/gui_app/weatherApp.py
+++ Python/gui_app/weatherApp.py
@@ -1,13 +1,26 @@
 import tkinter as tk
+import requests
 
 #window measurements
 height = 500
 width = 750
 
+def get_weather(city):
+    weather_key = 'c16c981aa0f2f29ca953e01dea70a72e'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city, 'APPID': weather_key, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    print(response.json())
+
+
 root = tk.Tk()
 
 canvas = tk.Canvas(height=height, width=width)
 canvas.pack()
+
+# background_image = tk.PhotoImage(file='background.png')
+# background_label = tk.Label(root, image=background_image)
+# background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg='black', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
@@ -15,13 +28,13 @@ frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 entry = tk.Entry(frame, font=75)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text='Click for weather...', font=75)
+button = tk.Button(frame, text='Click for weather...', font=75, command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relwidth=.3, relheight=1)
 
-lower_frame = tk.Frame(root, bg='black', bd=2.5)
+lower_frame = tk.Frame(root, bg='black', bd=5)
 lower_frame.place(relx=.5, rely=.25, relwidth=.75, relheight=.6, anchor='n')
 
-# label = tk.Label(frame, text='This is a label', bg='yellow')
-# label.pack()
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
 
 root.mainloop()
